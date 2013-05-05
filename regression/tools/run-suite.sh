b446bba944b683650012a6484dd312894f88b811
Phil Whineray <phil@sanewall.org>
2013-05-05 19:26:05 +0100

    Add regression test signing infrastructure

--- regression/tools/run-suite.sh
+++ regression/tools/run-suite.sh
@@ -136,26 +136,59 @@ mv /tmp/sanewall-tests-list.$$.srt /tmp/sanewall-tests-list.$$
 while read testfile
 do
   i=`echo $testfile | sed -e 's;/;-;g' -e s'/.conf$//'`
+  cfgfile="$outdir/$i.conf"
   logfile="$outdir/$i.log"
   v4out="$outdir/$i.out"
   v6out="$outdir/ipv6/$i.out"
+  v4aud="$outdir/$i.aud"
+  v6aud="$outdir/ipv6/$i.aud"
   v4nnout="$outdir/ipv4-no-nat/$i.out"
   echo ""
-  echo "  Running $testfile"
+  echo "  Running $cfgfile"
+  if grep -q "^====" "$here/$testfile"
+  then
+    audit="Y"
+    sigsfile="`echo $testfile | sed -e 's/.conf/.sigs/'`"
+    gpg --verify "$here/$sigsfile" "$here/$testfile"
+    sed -e '/^====/,$d' "$here/$testfile" > "$cfgfile"
+    sed -e '1,/^==== IPv4 AUDITED O/d' \
+        -e '/^==== IPv4 AUDITED END/,$d' "$here/$testfile" > "$v4aud"
+    if grep -q RESERVED_IPV6 "$prog"
+    then
+      audit6="Y"
+      sed -e '1,/^==== IPv6 AUDITED O/d' \
+          -e '/^==== IPv6 AUDITED END/,$d' "$here/$testfile" > "$v6aud"
+    else
+      audit6=""
+    fi
+  else
+    audit=""
+    audit6=""
+    cp "$here/$testfile" "$cfgfile"
+  fi
   echo "      log $logfile"
   echo " v4result $v4out"
   echo " v6result $v6out"
   clear_all
-  $prog "$here/$testfile" start >> "$logfile" 2>&1
+  $prog "$cfgfile" start >> "$logfile" 2>&1
   iptables-save > "$v4out"
   ip6tables-save > "$v6out"
   sed -i -e 's/^Sanewall: //' -e 's/^FireHOL: //' "$logfile"
+  sed -i -e '/^Processing file/s/ output\/[^/]*\// /' "$logfile"
   sed -i -e 's;/sbin/iptables;iptables_cmd;' "$logfile"
   sed -i -e 's/^# .*/#/' -e '/^:/s/\[[0-9][0-9]*:[0-9][0-9]*\]$/[0:0]/' "$v4out"
   sed -e '1d' -e '/^\*nat/,/^#/{d}' "$v4out" > "$v4nnout"
   sed -i -e 's/^# .*/#/' -e '/^:/s/\[[0-9][0-9]*:[0-9][0-9]*\]$/[0:0]/' "$v6out"
   sed -i -e 's/icmp6-/icmp-/' "$v6out"
   sed -i -e 's;::\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)/128;\1/32;' "$v6out"
+  if [ "$audit" ]
+  then
+    cmp "$v4out" "$v4aud" || echo "Warning: output differs from audited version"
+  fi
+  if [ "$audit6" ]
+  then
+    cmp "$v6out" "$v6aud" || echo "Warning: output differs from audited version"
+  fi
 done < /tmp/sanewall-tests-list.$$
 
 iptables-restore < /tmp/sanewall-tests-save.$$
